77af14f1c7c2319a422331dc411903e753301959
monclubelec <support@mon-club-elec.fr>
2013-06-22 16:52:58 +0200

    Create Blink.py

---
+++ pcduino/exemples/1.Basics/Blink.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# exemple pyDUino - par X. HINAULT - www.mon-club-elec.fr
+# voir : https://github.com/sensor56/pyDuino
+
+# LED clignote
+
+from arduino import * # importe les fonctions Arduino pour Python
+
+# entete declarative
+LED=2  # declare la broche a utiliser
+
+#--- setup --- 
+def setup():
+  pinMode(LED,OUTPUT) # met la broche en sortie
+	Serial.println("La broche " +str(LED)+ " est en sortie !")
+
+# -- fin setup -- 
+
+# -- loop -- 
+def loop():
+	
+	digitalWrite(LED,HIGH) # allume la LED
+	Serial.println("La LED est allumée !") 
+	
+	delay(1000) # pause en millisecondes
+	
+	digitalWrite(LED,LOW) # eteint la LED
+	Serial.println("La LED est éteinte !") 
+	
+	delay(1000) # pause en millisecondes
+	
+
+# -- fin loop --
+
+#--- obligatoire pour lancement du code -- 
+if __name__=="__main__": # pour rendre le code executable 
+	setup() # appelle la fonction main
+	while(1): loop() # appelle fonction loop sans fin
+
+
+
+
