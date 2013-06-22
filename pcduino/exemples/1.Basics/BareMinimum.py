40f1fcb4c75a0c67943495f263decbea8166479a
monclubelec <support@mon-club-elec.fr>
2013-06-22 13:20:44 +0300

    Create BareMinimum.py

---
+++ pcduino/exemples/1.Basics/BareMinimum.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# exemple pyDUino - par X. HINAULT - www.mon-club-elec.fr
+# voir : https://github.com/sensor56/pyDuino
+
+# code minimal
+
+from arduino import * # importe les fonctions Arduino pour Python
+
+# entete declarative
+
+#--- setup --- 
+def setup():
+  return 
+
+# -- fin seutp -- 
+
+# -- loop -- 
+def loop():
+	return
+
+# -- fin loop --
+
+#--- obligatoire pour lancement du code -- 
+if __name__=="__main__": # pour rendre le code executable 
+	setup() # appelle la fonction main
+	while(1): loop() # appelle fonction loop sans fin
