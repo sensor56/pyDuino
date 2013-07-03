9f4f813f3ebd424cb0d3c077a02d3080336b28ef
monclubelec <support@mon-club-elec.fr>
2013-07-03 13:52:40 +0200

    Update pyduino.py

--- pcduino/pyduino.py
+++ pcduino/pyduino.py
@@ -4,8 +4,26 @@
 # par X. HINAULT - Tous droits réservés - 2013
 # www.mon-club-elec.fr - Licence GPLv3
 
-""""
+"""
+// ------- Licence du code de ce programme -----
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License,
+//  or any later version.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Voir : http://www.mon-club-elec.fr/pmwiki_reference_pyduino/pmwiki.php?n=Main.Licence
+
+"""
+
+"""
 Ce fichier est partie intégrante  du projet pyDuino.
+Site officiel : http://www.mon-club-elec.fr/pmwiki_reference_pyduino/pmwiki.php?n=Main.HomePage
 
 pyDuino apporte une couche d'abstraction au langage Python 
 afin de pouvoir utiliser les broches E/S de mini PC
@@ -14,164 +32,115 @@ avec des instructions identiques au langage Arduino
 L'utilisation se veut la plus simple possible :
 un seul fichier à installer. 
 
-L'editeur conseille pour l'edition des codes Pyduino est Geany
-A installer dans un Terminal avec la commande
-$ sudo apt-get install geany
+Ce fichier est la version pour le raspberryPi version B
 
-Ce fichier est la version 0.1c pour le pcDuino
 """
-
 # modules utiles 
-
-#-- temps --
 import time
-import datetime # gestion date 
+import subprocess
 
-#-- math -- 
 # import math
 from math import *  # pour acces direct aux fonctions math..
 import random as rd # pour fonctions aléatoires - alias pour éviter problème avec fonction arduino random()
 
-#-- pour PWM - accès kernel + transposition C to Python -- 
-import fcntl # module pour fonction ioctl
-#from ctypes import *
-import ctypes # module pour types C en Python 
+# on presuppose ici que wiringPi est present sur le systeme: 
+# sudo apt-get install git 
+# git clone git://git.drogon.net/wiringPi
+# cd wiringPi
+# git pull origin
+# ./build
+
+# doc de gpio ici : http://wiringpi.com/the-gpio-utility/
 
 # -- declarations --
-# NB : les variables déclarées ici ne sont pas modifiables en dehors du module
-# pour modifier la valeur d'une variable de ce module, la seule solution est de la réaffecter dans le programme 
-# par exemple noLoop
 
+# sur le raspberryPi, la plupart des operations sont accessible avec la commande gpio 
 
-# sur le pcDuino, la plupart des operations passent par des fichiers systeme
-
-# fichiers broches E/S pcDuino
-pathMode="/sys/devices/virtual/misc/gpio/mode/"
-pathState="/sys/devices/virtual/misc/gpio/pin/"
+# fichiers broches E/S raspberryPi
+pathMain="/sys/class/gpio/gpio"
+pinList=['17', '18', '27', '22', '23', '24', '25', '4'] # definition des borches I/O - version B
+#pin=['17', '18', '21', '22', '23', '24', '25', '4'] # definition des borches I/O - version A
 
 # constantes Arduino like
-INPUT="0"
-OUTPUT="1"
-PULLUP="8"
+INPUT="in"
+OUTPUT="out"
+PULLUP="up" # accepte par commande gpio
 
 HIGH = 1
 LOW =  0
 
 A0, A1, A2, A3, A4,A5 =0,1,2,3,4,5 # identifiant broches analogiques
-PWM0, PWM1, PWM2, PWM3, PWM4,PWM5 =3,5,6,9,10,11 # identifiant broches PWM
+PWM0=1 # identifiant broches PWM
 
-DEC=10
-BIN=2
-HEX=16
-OCT=8
+# constantes Pyduino
+noLoop=False 
 
-# constantes utiles pyDuino
-noLoop=False # pour stopper loop
+# --- fonctions Arduino ---- 
 
-# -- pour PWM --
-
-# le chemin fichier config PWM
-pwm_dev = "/dev/pwmtimer"
-
-# les adresses registres PWM 
-PWMTMR_START =0x101
-PWMTMR_STOP =0x102
-PWMTMR_FUNC=0x103
-PWMTMR_TONE =0x104
-PWM_CONFIG=0x105
-HWPWM_DUTY=0x106
-PWM_FREQ=0x107
-
-# Les frequences max
-MAX_PWMTMR_FREQ=2000 # 2kHz pin 3,9,10,11 
-MIN_PWMTMR_FREQ=126 # 126Hz pin 3,9,10,11 
-MAX_PWMHW_FREQ=20000 # 20kHz pin 5,6
-
-MAX_PWM_LEVEL=255 # limite max duty cycle
-
-initPwmFlag=[False,False,False,False,False, False] # flags init Freq PWM - false tant que pas initialisation freq PWM
-defaultPwmFreq=520 # frequence par defaut
-
-#==== diverses classes utiles utilisées par les fonctions Pyduino ===
-
-# -- pour config PWM -- 
-
-"""	 
-	 typedef struct tagPWM_Config {
-    int channel;
-    int dutycycle;
-} PWM_Config,*pPWM_Config;
-"""
-
-class PWM_Config(ctypes.Structure):
-    _fields_ = [
-        ('channel', ctypes.c_int),
-        ('dutycycle', ctypes.c_int)
-    ]
-
-"""
-typedef struct tagPWM_Freq {
-    int channel;
-    int step;
-    int pre_scale;
-    unsigned int freq;
-} PWM_Freq,*pPWM_Freq;
-"""
-
-class PWM_Freq(ctypes.Structure):
-    _fields_ = [
-        ('channel', ctypes.c_int),
-        ('step', ctypes.c_int),
-        ('pre_scale', ctypes.c_int),
-        ('freq', ctypes.c_uint)
-    ]
-
-
-# ==================== Fonctions spécifiques pour une plateforme donnée =============================
-# =====================>>>>>>>>>> version pcDuino <<<<<<<<<<< =======================================
-
-
-# ---- gestion broches E/S numériques ---
+#====================== fonctions specifiques de la plateforme ===================
+#========================= fonctions RaspberryPi =================================
 
 # pinMode 
 def pinMode(pin, mode):
-	# fixe le mode de fonctionnement de la broche
-	# mode parmi : OUTPUT, INPUT ou PULLUP
 	
 	pin=int(pin) # numéro de la broche (int)
 	mode=str(mode) # mode de fonctionnement (str)
 	
-	# fixe le mode de la broche E/S
-	file=open(pathMode+"gpio"+str(pin),'w') # ouvre le fichier en ecriture
-	file.write(mode) # ecrit dans le fichier le mode voulu
-	file.close()
+	# gpio mode <pin> in/out/pwm/clock/up/down/tri
+	
+	if mode==INPUT or mode==OUTPUT : # si in ou out 
+		# en acces direct = plus rapide 
+		file=open(pathMain+pinList[pin]+"/direction",'w') # ouvre le fichier en écriture
+		file.write(OUTPUT)
+		file.close()
+	elif mode==PULLUP : # sinon = si up
+		# fixe le mode de la broche E/S via ligne commande gpio 
+		cmd="gpio mode "+str(pin)+" "+mode
+		subprocess.Popen(cmd, shell=True)
+		print cmd # debug
+	
 
 # digitalWrite 
 def digitalWrite(pin, state):
-	# met la broche dans l'état voulu
-	# state parmi : HIGH ou LOW
 	
 	pin=int(pin)
 	state=str(state) # transforme en chaine
 	
-	# met la broche dans etat voulu
-	file=open(pathState+"gpio"+str(pin),'w') # ouvre le fichier en ecriture
+	# gpio mode <pin> in/out/pwm/clock/up/down/tri
+	
+	# met la broche dans etat voulu via ligne de commande gpio
+	#cmd="gpio write "+str(pin)+" "+str(state)
+	#subprocess.Popen(cmd, shell=True)	
+	#print cmd # debug
+	
+	# en acces direct = plus rapide 
+	file=open(pathMain+pinList[pin]+"/value",'w') # ouvre le fichier en écriture
 	file.write(state)
 	file.close()
 	
 
+
 # digitalRead
 def digitalRead(pin):
-	# lit l'état de la broche numérique
-	# renvoie int : LOW ou HIGH
 	
 	pin=int(pin)
 	
-	# lit etat de la broche 
-	file=open(pathState+"gpio"+str(pin),'r') # ouvre le fichier en lecture
+	# gpio read <pin>
+	
+	# lit l'etat de la broche en ligne commande avec gpio
+	#cmd="gpio read "+str(pin)
+	#print cmd # debug
+	
+	#pipe=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout 
+	#state=pipe.read() # lit la sortie console
+	#pipe.close() # ferme la sortie console
+
+	#print state # debug
+	
+	# lit etat de la broche en acces direct 
+	file=open(pathMain+pinList[pin]+"/value",'r') # ouvre le fichier en lecture
 	file.seek(0) # se place au debut du fichier
-	state=file.read()  #lit le fichier
-	#print state #debug
+	state=file.read() #lit le fichier
 	file.close()
 	
 	return int(state)  # renvoie valeur entiere
@@ -185,184 +154,39 @@ def toggle(pin): # inverse l'etat de la broche
 		digitalWrite(pin,HIGH)
 		return HIGH
 
-#----- gestion broches analogique -----
+#------------- Broches analogiques -----------------
 
-# analogRead - entrées analogiques 
-def analogRead(pinAnalog):
-	# A0 et A1 : résolution 6 bits (0-63) en 0-2V
-	# A2, A3, A4, A5 : résolution 12 bits (0-4095) en 0-3.3V
+# analogRead
+def analogRead(pin):
 	
-	pin=int(pinAnalog) # pin est un int entre 0 et 5 - utilisation identifiant predefini possible
+	print("analogRead non disponible sur le RaspberryPi : passer au pcDuino !")
 	
-	# lecture du fichier
-	file=open('/proc/adc'+str(pinAnalog),'r')
-	file.seek(0)  # en debut du fichier
-	out=file.read() # lit la valeur = un str de la forme adc 0 : valeur
-	file.close()
-	
-	# extraction de la valeur
-	out=out.split(":") # scinde en 2 la chaine "adc 0 : valeur"
-	out=out[1] # garde la 2eme partie = la valeur
-	
-	return int(out) # renvoie la valeur
+	return 0 # renvoie la valeur
 
-# analogReadmV - entrées analogiques - renvoie valeur en millivolts
-def analogReadmV(pinAnalog):
-	# A0 et A1 : résolution 6 bits (0-63) en 0-2V
-	# A2, A3, A4, A5 : résolution 12 bits (0-4095) en 0-3.3V
+# analogWrite = generation pwm
+def analogWrite(pin, value): 
 	
-	mesure=analogRead(pinAnalog)
-	if pinAnalog==A0 or pinAnalog==A1:
-		mesure=rescale(mesure,0,63,0,2000)
-	elif pinAnalog==A2 or pinAnalog==A3 or pinAnalog==A4 or pinAnalog==A5:
-		mesure=rescale(mesure,0,4095,0,3300)
+	pin=int(pin)
+	value=int(rescale(value,0,255,0,1023))
 	
-	return mesure
-
-# setFrequence - fixe fréquence PWM 
-# D'après : https://github.com/pcduino/c_enviroment/blob/master/hardware/arduino/cores/arduino/wiring_analog.c 
-# adaptation C to Python by X. HINAULT - Juin 2013
-def setFrequencyPWM(pinPWMIn, frequencePWMIn):
-	# broches PWM 3/9/10/11 supporte frequences[125-2000]Hz à differents dutycycle
-	# broches PWM 5/6 supporte frequences [195,260,390,520,781]Hz à 256 dutycycle
+	# fixe le mode pwm pour la broche E/S via ligne commande gpio 
+	cmd="gpio mode "+str(pin)+" "+"pwm"
+	subprocess.Popen(cmd, shell=True)
+	print cmd # debug
 	
-	global initPwmFlag
+	# gpio pwm <pin> <value> avec value entre 0 et 1023
 	
-	pin=ctypes.c_int(pinPWMIn) # broche
-	freq=ctypes.c_uint(frequencePWMIn) #frequence 
-	# ATTENTION : la valeur ctype ne pourra pas etre utilisee comme une valeur Python... 
+	# fixe pwm via ligne commande gpio 
+	cmd="gpio pwm "+str(pin)+" "+ str(value)
+	subprocess.Popen(cmd, shell=True)
+	print cmd # debug
 	
-	#print pin
-	# attention : utiliser pinPWMIn pour les conditions - pin est c_type.
+def analogWritePercent(pin, value):
+	analogWrite(pin, rescale(value,0,100,0,255)) # re-echelonne valeur 0-100% vers 0-255
 	
-	pwmfreq = PWM_Freq() # declare objet structure
-	
-	if (pinPWMIn==3 or pinPWMIn==5 or pinPWMIn==6 or pinPWMIn==9 or pinPWMIn==10 or pinPWMIn==11) and frequencePWMIn>0 :
-		pwmfreq.channel=pin
-		pwmfreq.freq=freq
-		pwmfreq.step=0
-		
-		# ouverture fichier
-		fd=open(pwm_dev,'r')
-		
-		if pinPWMIn==5 or pinPWMIn==6 : # si broches 5 et 6 
-			# pin(5/6) support frequency[195,260,390,520,781] @256 dutycycle
-			if frequencePWMIn==195 or frequencePWMIn==260 or frequencePWMIn==390 or frequencePWMIn==520 or frequencePWMIn==781: 
-				ret=fcntl.ioctl(fd, PWM_FREQ, pwmfreq)  # fixe la frequence voulue 
-				#initPwmFlag[pinPWMIn]=True # flag temoin config freq PWM mis à True
-				initPwmFlag[pinPWMIn]= frequencePWMIn # flag temoin config freq PWM mis à valeur courante freq
-				#print ret # debug
-				if ret<0 :
-					print ("Problème lors configuration PWM")
-					if fd : fd.close()
-					return
-			else:
-				print("Fréquence incompatible : choisir parmi 195,260,390,520,781 Hz")
-			
-		elif pinPWMIn==3 or pinPWMIn==9 or pinPWMIn==10 or pinPWMIn==11 : 
-			# broches PWM 3/9/10/11 supporte frequences[125-2000]Hz à differents dutycycle
-			if frequencePWMIn >= MIN_PWMTMR_FREQ and frequencePWMIn <= MAX_PWMTMR_FREQ :  # si freq entre 126 et 2000Hz
-				
-				# -- stop pwmtmr sur broche ---
-				ret=fcntl.ioctl(fd, PWMTMR_STOP, ctypes.c_ulong(pwmfreq.channel))
-				#print ret
-				if ret<0 :
-					print ("Probleme lors arret PWM")
-					if fd : fd.close()
-					return
-				
-				# -- fixe frequence pwm
-				ret=fcntl.ioctl(fd, PWM_FREQ, pwmfreq)
-				#initPwmFlag[pinPWMIn]=True # flag temoin config freq PWM mis à True
-				initPwmFlag[pinPWMIn]= frequencePWMIn # flag temoin config freq PWM mis à valeur courante freq
-				#print ret
-				if ret<0 :
-					print ("Probleme lors configuration PWM")
-					if fd : fd.close()
-					return
-				
-			
-		
-		if fd : fd.close() # ferme fichier si existe 
-	else : print("Broche non autorisee pour PWM")
-	
-
-# analogWrite - sortie analogique = PWM
-def analogWriteHardware(pinPWMIn, largeurIn):
-	
-	global initPwmFlag
-		
-	if initPwmFlag[pinPWMIn]==False :  # si frequence PWM pas initialisee
-		setFrequencyPWM(pinPWMIn,defaultPwmFreq) # utilise la frequence PWM par defaut 
-	
-	pin=ctypes.c_int(pinPWMIn) # broche
-	value=ctypes.c_int(largeurIn)  # largeur impulsion
-	
-	pwmconfig = PWM_Config() # declare objet structure like C  voir classe debut code 
-	
-	pwmconfig.channel=pin
-	pwmconfig.dutycycle=value
-	
-	
-	if (pinPWMIn==3 or pinPWMIn==5 or pinPWMIn==6 or pinPWMIn==9 or pinPWMIn==10 or pinPWMIn==11) and  largeurIn>=0 and largeurIn <= MAX_PWM_LEVEL : 
-	# utiliser largeurIn car value est ctype...
-		
-		# ouverture fichier
-		fd=open(pwm_dev,'r')
-		
-		if pinPWMIn==5 or pinPWMIn==6 : # si broches 5 et 6 
-			
-			# -- fixe largeur pwm
-			ret=fcntl.ioctl(fd, HWPWM_DUTY, pwmconfig)
-			#print ret
-			if ret<0 :
-				print ("Probleme lors configuration HWPWM_DUTY")
-				if fd : fd.close()
-				return
-			
-		elif pinPWMIn==3 or pinPWMIn==9 or pinPWMIn==10 or pinPWMIn==11 : 
-			# -- fixe largeur pwm
-			ret=fcntl.ioctl(fd, PWM_CONFIG, pwmconfig)
-			#print ret
-			if ret<0 :
-				print ("Probleme lors configuration PWM")
-				if fd : fd.close()
-				return
-				
-			# -- démarre pwmtmr sur broche ---
-			ret=fcntl.ioctl(fd, PWMTMR_START, ctypes.c_ulong(0))
-			#print ret
-			if ret<0 :
-				print ("Probleme lors configuration PWM")
-				if fd : fd.close()
-				return
-		
-		if fd : fd.close() # ferme fichier si existe 
-	else : print("Broche non autrisee pour PWM")
-	
-
-# analogWrite # idem Arduino en 0-255
-def analogWrite(pinPWMIn, largeurIn):
-	
-	global initPwmFlag
-	
-	if initPwmFlag[pinPWMIn]==False :  # si frequence PWM pas initialisee
-		setFrequencyPWM(pinPWMIn,defaultPwmFreq) # utilise la frequence PWM par defaut 
-
-	if pinPWMIn==3 or pinPWMIn==9 or pinPWMIn==10 or pinPWMIn==11: # duty natif 0-60 pour ces  broches en 520Hz
-		maxDuty=int(33000/initPwmFlag[pinPWMIn]) # initPwmFlag contient la valeur de la freq courante
-		
-		largeurIn=rescale(largeurIn,0,255,0,maxDuty) # rescale 0-255 vers 0-maxDuty
-	#elif pinPWMIn==5 or pinPWMIn==6: # duty natif 0-255 pour ces 2 broches = inchangé
-	
-	analogWriteHardware(pinPWMIn, largeurIn) # appelle fonction utilisant largeur Hardware
-
-# analogWritePercent(pinPWMIn, largeurIn)=> rescale 0-100 vers 0-255
-def analogWritePercent(pinPWMIn, largeurIn):
-	analogWrite(pinPWMIn,rescale(largeurIn,0,100,0,255))
-	
-
-################ Fonctions communes ####################
+#############################################################################
+#==================== Fonctions Pyduino communes ============================
+##############################################################################
 
 #--- temps ---
  
@@ -392,44 +216,6 @@ def microsSyst():
 def micros():
 	return microsSyst()-micros0Syst # renvoie difference entre microsSyst courant et microsSyst debut code
 	
-
-# --- fonctions date - RTC - unixtime 
-def year():
-	return str(datetime.datetime.now().year)
-
-def month():
-	if datetime.datetime.now().month<10:
-		return "0"+str(datetime.datetime.now().month) # ajoute 0 si < 10
-	else:
-		return str(datetime.datetime.now().month)
-
-def day():
-	if datetime.datetime.now().day<10:
-		return "0"+str(datetime.datetime.now().day) # ajoute 0 si < 10
-	else:
-		return str(datetime.datetime.now().day)
-
-def hour():
-	if datetime.datetime.now().hour<10:
-		return "0"+str(datetime.datetime.now().hour) # ajoute 0 si < 10
-	else:
-		return str(datetime.datetime.now().hour)
-
-def minute():
-	if datetime.datetime.now().minute<10:
-		return "0"+str(datetime.datetime.now().minute) # ajoute 0 si < 10
-	else:
-		return str(datetime.datetime.now().minute)
-
-def second():
-	if datetime.datetime.now().second<10:
-		return "0"+str(datetime.datetime.now().second) # ajoute 0 si < 10
-	else:
-		return str(datetime.datetime.now().second)
-
-def unixtime():
-	return str(int(time.time()))
-
 
 #----------- MATH -------------
 
@@ -481,8 +267,6 @@ def randomSeed(x):
 	
 #-- random(max) et random(min,max) : renvoie valeur aléatoire entière
 def random(*arg): # soit forme random(max), soit forme random(min,max)
-	# Renvoie une valeur aléatoire entiere
-	
 	if len(arg)==1:
 		return rd.randint(0,arg[0])
 	elif len(arg)==2:
@@ -614,7 +398,7 @@ class Serial():
 
 # fin classe Serial 
 
-########################### --------- initialisation ------------ #################
+########################### initialisation ########################
 
 Serial = Serial() # declare une instance Serial pour acces aux fonctions depuis code principal
 
