5d9b75b23d3ad0e3bb93b09f3f8130a217e96511
Poztit <poztit42@gmail.com>
2015-02-13 22:38:55 +0100

    Agencement Pyduino_rpi.py

--- pyduino_rpi.py
+++ pyduino_rpi.py
@@ -1,13 +1,13 @@
-# par F. ILLIEN - Tous droits réservés - 2015
+# Par F. ILLIEN - Tous droits réservés - 2015
 # www.mon-club-elec.fr - Licence GPLv3
 
 print "Pyduino for Raspberry Pi - by www.mon-club-elec.fr - 2015 "
 
 
-#### expressions regulieres ####
-import re # expression regulieres pour analyse de chaines
+### Expressions regulieres ###
+import re # Expression regulieres pour analyse de chaines
 
-# serie
+# Serie
 try:
 	import serial
 except:
@@ -19,10 +19,10 @@ import socket
 import smtplib # serveur mail 
 """
 
-#--- module des variables communes partagées entre les éléments Pyduino -- 
+### Module des variables communes partagées entre les éléments Pyduino ###
 import CoreCommon as common
 
-# -- declarations --
+##### declarations #####
 # NB : les variables déclarées ici ne sont pas modifiables en dehors du module
 # pour modifier la valeur d'une variable de ce module, la seule solution est de la réaffecter dans le programme 
 # par exemple noLoop ou de passer par un fichier commun... 
@@ -32,28 +32,28 @@ import CoreCommon as common
 
 common.PLATFORM = "RPI"
 
-# fichiers broches E/S raspberryPi
+# Fichiers broches E/S raspberryPi
 pathMain = "/sys/class/gpio/gpio/"
 
-pinList = ['17', '18', '27', '22', '23', '24', '25', '4'] # definition des borches I/O - version B
-#pin=['17', '18', '21', '22', '23', '24', '25', '4'] # definition des borches I/O - version A
+pinList = ['17', '18', '27', '22', '23', '24', '25', '4'] # Definition des borches I/O - version B
+#pin=['17', '18', '21', '22', '23', '24', '25', '4'] # Definition des borches I/O - version A
 
-A0, A1, A2, A3, A4, A5 = 0, 1, 2, 3, 4, 5 # identifiant broches analogiques
-PWM0 = 1 # identifiant broches PWM
+A0, A1, A2, A3, A4, A5 = 0, 1, 2, 3, 4, 5 # Identifiant broches analogiques
+PWM0 = 1 # Identifiant broches PWM
 
 
-# constantes Arduino like spécifique de la plateforme utilisée 
+# Constantes Arduino like spécifique de la plateforme utilisée 
 common.INPUT = "in"
 common.OUTPUT = "out"
-common.PULLUP = "up" # accepter par la commande gpio
+common.PULLUP = "up" # Accepter par la commande gpio
 
-#### les sous modules Pyduino utilisés par ce module ####
+### Les sous modules Pyduino utilisés par ce module ###
 from CoreBase import *
 from CoreSystem import *
 from CoreLibs import *
-#### pour PWM - accès kernel + transposition C to Python ####
-import fcntl # module pour fonction ioctl
-import ctypes # module pour types C en Python
+### Pour PWM - accès kernel + transposition C to Python ###
+import fcntl # Module pour fonction ioctl
+import ctypes # Module pour types C en Python
 
 
 ############## Broche logique ##############
@@ -61,8 +61,8 @@ import ctypes # module pour types C en Python
 # export
 def export(pin):
 	try:
-		file = open(pathMain + "export", 'w')
-		file.write(pinList[pin])
+		file = open(pathMain + "export", 'w') # Ouvre le fichier en ecriture 
+		file.write(pinList[pin]) # Ecrie le pin a exporter
 		file.close()
 	except:
 		print "ERREUR : Impossible d'ouvrir la broche"
@@ -72,16 +72,16 @@ def export(pin):
 
 # pinMode 
 def pinMode(pin, mode):
-	pin = int(pin) # numéro de la broche (int)
-	mode = str(mode) # mode de fonctionnement (str)
+	pin = int(pin) # Numero de la broche (int)
+	mode = str(mode) # Mode de fonctionnement (str)
 	
 	if export(pin) == 0:
 		# gpio mode <pin> in/out/pwm/clock/up/down/tri
-		if mode == INPUT or mode == OUTPUT : # si in ou out 
-			# en acces direct = plus rapide 
+		if mode == INPUT or mode == OUTPUT : # Si in ou out 
+			# En acces direct = plus rapide 
 			try:
-				file = open(pathMain + "gpio" + pinList[pin] + "/direction",'w') # ouvre le fichier en écriture
-				file.write(OUTPUT)
+				file = open(pathMain + "gpio" + pinList[pin] + "/direction",'w') # Ouvre le fichier en ecriture
+				file.write(OUTPUT) # Ecrie l'etat du pin demande
 				file.close()
 			except:
 				print "ERREUR : Impossible d'orienté la broche"
@@ -89,8 +89,8 @@ def pinMode(pin, mode):
 			else:
 				return 0
 
-		elif mode == PULLUP : # sinon = si up
-			# fixe le mode de la broche E/S via ligne commande gpio 
+		elif mode == PULLUP : # Sinon = si up
+			# Fixe le mode de la broche E/S via ligne commande gpio 
 			cmd = "gpio mode " + str(pin) + " " + mode
 			subprocess.Popen(cmd, shell = True)
 			print cmd # debug
@@ -106,18 +106,18 @@ def pinMode(pin, mode):
 # digitalWrite 
 def digitalWrite(pin, state):
 	pin = int(pin)
-	state = str(state) # transforme en chaine
+	state = str(state) # Transforme en chaine
 	
 	# gpio mode <pin> in/out/pwm/clock/up/down/tri
 	
-	# met la broche dans etat voulu via ligne de commande gpio
-	#cmd="gpio write "+str(pin)+" "+str(state)
-	#subprocess.Popen(cmd, shell=True)	
-	#print cmd # debug
+	# Met la broche dans etat voulu via ligne de commande gpio
+	# cmd="gpio write "+str(pin)+" "+str(state)
+	# subprocess.Popen(cmd, shell=True)	
+	# print cmd # debug
 	
-	# en acces direct = plus rapide 
+	# En acces direct = plus rapide 
 	try:
-		file = open(pathMain + "gpio" + pinList[pin] + "/value",'w') # ouvre le fichier en écriture
+		file = open(pathMain + "gpio" + pinList[pin] + "/value",'w') # Ouvre le fichier en écriture
 		file.write(state)
 		file.close()
 	except:
@@ -131,18 +131,18 @@ def digitalRead(pin):
 	pin = int(pin)
 
     try:
-		# lit etat de la broche en acces direct 
-		file = open(pathMain + "gpio" + pinList[pin] + "/value",'r') # ouvre le fichier en lecture
-		file.seek(0) # se place au debut du fichier
-		state = file.read() #lit le fichier
+		# Lit etat de la broche en acces direct 
+		file = open(pathMain + "gpio" + pinList[pin] + "/value",'r') # Ouvre le fichier en lecture
+		file.seek(0) # Se place au debut du fichier
+		state = file.read() # Lit le fichier
 		file.close()
 	except:
 		print "ERREUR : Impossible de lire la broche"
 		return -1
 	else:
-		return int(state)  # renvoie valeur entiere
+		return int(state)  # Renvoie valeur entiere
 
-def toggle(pin): # inverse l'etat de la broche
+def toggle(pin): # Inverse l'etat de la broche
 	if digitalRead(pin) == HIGH:
 		digitalWrite(pin,LOW)
 		return LOW
@@ -155,97 +155,97 @@ def toggle(pin): # inverse l'etat de la broche
 # analogRead
 def analogRead(pin):
 	print "ERREUR : analogRead non disponible sur le RaspberryPi"
-	return 0 # renvoie la valeur
+	return 0 # Renvoie la valeur
 
 # analogWrite = generation pwm
 def analogWrite(pin, value): 
 	pin = int(pin)
 	value = int(rescale(value,0,255,0,1023))
 	
-	# fixe le mode pwm pour la broche E/S via ligne commande gpio 
+	# Fixe le mode pwm pour la broche E/S via ligne commande gpio 
 	cmd = "gpio mode " + str(pin) + " " + "pwm"
 	subprocess.Popen(cmd, shell=True)
 	print cmd # debug
 	
 	# gpio pwm <pin> <value> avec value entre 0 et 1023
 	
-	# fixe pwm via ligne commande gpio 
+	# Fixe pwm via ligne commande gpio 
 	cmd = "gpio pwm " + str(pin) + " " + str(value)
 	subprocess.Popen(cmd, shell=True)
 	print cmd # debug
 	
 def analogWritePercent(pin, value):
-	analogWrite(pin, rescale(value,0,100,0,255)) # re-echelonne valeur 0-100% vers 0-255
+	analogWrite(pin, rescale(value,0,100,0,255)) # Re-echelonne valeur 0-100% vers 0-255
 	
 
 ######################## Fonctions Libs dédiées ################################
 
-# classe Uart pour communication série UART 
+# Classe Uart pour communication série UART 
 class Uart():
-	# def __init__(self): # constructeur principal
-	def begin(self,rateIn, *arg): # fonction initialisation port serie 
+	# def __init__(self): # Constructeur principal
+	def begin(self,rateIn, *arg): # Fonction initialisation port serie 
 		# arg = rien ou timeout ou timeout et port a utiliser
 		global uartPort
 		
-		# configure pin 0 et 1 pour UART (mode = 3)
-		#pinMode(RX,UART)
-		#pinMode(TX,UART)
+		# Configure pin 0 et 1 pour UART (mode = 3)
+		# pinMode(RX,UART)
+		# pinMode(TX,UART)
 		
-		#-- initialisation port serie uart 
+		### Initialisation port serie uart ###
 		#try:
-		if len(arg) == 0: # si pas d'arguments
-			#uartPort=serial.Serial('/dev/ttyS1', rateIn, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout = 10) # initialisation port serie uart
-			uartPort = serial.Serial('/dev/ttyAMA0', rateIn, timeout = 10) # initialisation port serie uart
-			print "Initialisation Port Serie : /dev/ttyAMA0 @ " + str(rateIn) + " = OK " # affiche debug
+		if len(arg) == 0: # Si pas d'arguments
+			# uartPort=serial.Serial('/dev/ttyS1', rateIn, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout = 10) # Initialisation port serie uart
+			uartPort = serial.Serial('/dev/ttyAMA0', rateIn, timeout = 10) # Initialisation port serie uart
+			print "Initialisation Port Serie : /dev/ttyAMA0 @ " + str(rateIn) + " = OK " # Affiche debug
 		elif len(arg) == 1 : # si timeout
-			#uartPort=serial.Serial('/dev/ttyS1', rateIn, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout = arg[0]) # initialisation port serie uart
-			uartPort = serial.Serial('/dev/ttyAMA0', rateIn, timeout = arg[0]) # initialisation port serie uart
-			print "Initialisation Port Serie : /dev/ttyAMA0 @ " + str(rateIn) + " = OK " # affiche debug
+			# uartPort=serial.Serial('/dev/ttyS1', rateIn, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout = arg[0]) # Initialisation port serie uart
+			uartPort = serial.Serial('/dev/ttyAMA0', rateIn, timeout = arg[0]) # Initialisation port serie uart
+			print "Initialisation Port Serie : /dev/ttyAMA0 @ " + str(rateIn) + " = OK " # Affiche debug
 			print "timeout = " + str(arg[0])
 		elif len(arg) == 2 : # si timeout et port 
-			#uartPort=serial.Serial('/dev/ttyS1', rateIn, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout = arg[0]) # initialisation port serie uart
-			uartPort=serial.Serial(arg[1], rateIn, timeout = arg[0]) # initialisation port serie uart
-			print "Initialisation Port Serie : " + arg[1] + " @ " + str(rateIn) + " = OK " # affiche debug
+			# uartPort=serial.Serial('/dev/ttyS1', rateIn, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout = arg[0]) # Initialisation port serie uart
+			uartPort=serial.Serial(arg[1], rateIn, timeout = arg[0]) # Initialisation port serie uart
+			print "Initialisation Port Serie : " + arg[1] + " @ " + str(rateIn) + " = OK " # Affiche debug
 			print "timeout = " + str(arg[0])
 		#except:
-		#	print ("Erreur lors initialisation port Serie") 
+		#	print "Erreur lors initialisation port Serie" 
 			
-	def println(self,text, *arg):  # message avec saut de ligne
+	def println(self,text, *arg):  # Message avec saut de ligne
 		# Envoi chaine sur port serie uart 
 		# Supporte formatage chaine façon Arduino avec DEC, BIN, OCT, HEX
 		
 		global uartPort
 		
-		# attention : arg est reçu sous la forme d'une liste, meme si 1 seul !
-		text = str(text) # au cas où
+		# Attention : arg est reçu sous la forme d'une liste, meme si 1 seul !
+		text = str(text) # Au cas où
 		# print "text =" + text # debug
 		
-		arg = list(arg) # conversion en list... évite problèmes.. 
+		arg = list(arg) # Conversion en list... évite problèmes.. 
 		
-		#print arg - debug
+		# print arg - debug
 		
-		if not len(arg) == 0: # si arg a au moins 1 element (nb : None renvoie True.. car arg existe..)
+		if not len(arg) == 0: # Si arg a au moins 1 element (nb : None renvoie True.. car arg existe..)
 			if arg[0] == DEC and text.isdigit():
 				out = text
-				#print(out) # debug
+				# print out # debug
 			elif arg[0] == BIN and text.isdigit():
 				out=bin(int(text))
-				#print(out) # debug
+				# print out # debug
 			elif arg[0] == OCT and text.isdigit():
 				out=oct(int(text))
-				#print(out) # debug
+				# print out # debug
 			elif arg[0] == HEX and text.isdigit():
 				out = hex(int(text))
-				#print(out) # debug
-		else: # si pas de formatage de chaine = affiche tel que 
+				# print out # debug
+		else: # Si pas de formatage de chaine = affiche tel que 
 			out = text
-			#print(out) # debug
+			# print out # debug
 		
 		uartPort.write(out + chr(10)) # + saut de ligne 
 		# print "Envoi sur le port serie Uart : " + out+chr(10) # debug
 		uartPort.flush()
-		# ajouter formatage Hexa, Bin.. cf fonction native bin... 
-		# si type est long ou int
+		# Ajouter formatage Hexa, Bin.. cf fonction native bin... 
+		# Si type est long ou int
 	
 	
 	def available(self):
@@ -270,67 +270,67 @@ class Uart():
 		
 	
 	
-	#--- lecture d'une ligne jusqu'a caractere de fin indique
-	def waiting(self, *arg): # lecture d'une chaine en reception sur port serie 
+	### Lecture d'une ligne jusqu'a caractere de fin indique ###
+	def waiting(self, *arg): # Lecture d'une chaine en reception sur port serie 
 		global uartPort
 		
-		if len(arg) == 0: endLine = "\n" # par defaut, saut de ligne
-		elif len(arg) == 1: endLine = arg[0] # sinon utilise caractere voulu
+		if len(arg) == 0: endLine = "\n" # Par defaut, saut de ligne
+		elif len(arg) == 1: endLine = arg[0] # Sinon utilise caractere voulu
 		
-		#-- variables de reception -- 
+		### Variables de reception ###
 		chaineIn = ""
 		charIn = ""
 		
-		#delay(20) # laisse temps aux caracteres d'arriver
+		# delay(20) # Laisse temps aux caracteres d'arriver
 		
-		while uartPort.inWaiting(): # tant que au moins un caractere en reception
-			charIn = uartPort.read() # on lit le caractere
-			#print charIn # debug
+		while uartPort.inWaiting(): # Tant que au moins un caractere en reception
+			charIn = uartPort.read() # On lit le caractere
+			# print charIn # debug
 			
-			if charIn == endLine: # si caractere fin ligne , on sort du while
-				#print("caractere fin de ligne recu") # debug
-				break # sort du while
-			else: #tant que c'est pas le saut de ligne, on l'ajoute a la chaine 
+			if charIn == endLine: # Si caractere fin ligne , on sort du while
+				# print("Caractere fin de ligne recu") # debug
+				break # Sort du while
+			else: # Tant que c'est pas le saut de ligne, on l'ajoute a la chaine 
 				chaineIn = chaineIn + charIn
 				# print chaineIn # debug
 			
-		#-- une fois sorti du while : on se retrouve ici - attention indentation 
+		### Une fois sorti du while : on se retrouve ici - attention indentation ###
 		if len(chaineIn) > 0: # ... pour ne pas avoir d'affichage si ""	
-			#print(chaineIn) # affiche la chaine # debug
-			return chaineIn  # renvoie la chaine 
+			# print(chaineIn) # Affiche la chaine # debug
+			return chaineIn  # Renvoie la chaine 
 		else:
-			return False # si pas de chaine
+			return False # Si pas de chaine
 	
-	#--- lecture de tout ce qui arrive en réception 
-	def waitingAll(self): # lecture de tout en reception sur port serie 
+	### Lecture de tout ce qui arrive en réception ###
+	def waitingAll(self): # Lecture de tout en reception sur port serie 
 		global uartPort
 		
-		#-- variables de reception -- 
+		### Variables de reception ###
 		chaineIn = ""
 		charIn = ""
 		
-		#delay(20) # laisse temps aux caracteres d'arriver
+		# delay(20) # Laisse temps aux caracteres d'arriver
 		
-		while uartPort.inWaiting(): # tant que au moins un caractere en reception
-			charIn = uartPort.read() # on lit le caractere
-			#print charIn # debug
+		while uartPort.inWaiting(): # Tant que au moins un caractere en reception
+			charIn = uartPort.read() # On lit le caractere
+			# print charIn # debug
 			chaineIn = chaineIn + charIn
 			# print chaineIn # debug
 			
-		#-- une fois sorti du while : on se retrouve ici - attention indentation 
+		### Une fois sorti du while : on se retrouve ici - attention indentation ###
 		if len(chaineIn) > 0: # ... pour ne pas avoir d'affichage si ""	
-			#print(chaineIn) # affiche la chaine # debug
-			return chaineIn  # renvoie la chaine 
+			#print(chaineIn) # Affiche la chaine # debug
+			return chaineIn  # Renvoie la chaine 
 		else:
-			return False # si pas de chaine
+			return False # Si pas de chaine
 
-# ajouter write / read   / flush
+# Ajouter write / read   / flush
 
-########################### --------- initialisation ------------ #################
+########################### --------- Initialisation ------------ #################
 
-Serial = Serial() # declare une instance Serial pour acces aux fonctions depuis code principal
-Ethernet = Ethernet() # declare instance Ethernet implicite pour acces aux fonctions 
-Uart = Uart() # declare instance Uart implicite 
+Serial = Serial() # Declare une instance Serial pour acces aux fonctions depuis code principal
+Ethernet = Ethernet() # Declare instance Ethernet implicite pour acces aux fonctions 
+Uart = Uart() # Declare instance Uart implicite 
 
-micros0Syst = microsSyst() # mémorise microsSyst au démarrage
-millis0Syst = millisSyst() # mémorise millisSyst au démarrage
+micros0Syst = microsSyst() # Mémorise microsSyst au démarrage
+millis0Syst = millisSyst() # Mémorise millisSyst au démarrage
